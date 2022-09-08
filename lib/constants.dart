3eee0626be72cd415cd3feffd3208bf874de6c4b
JasonLim1009 <107684179+JasonLim1009@users.noreply.github.com>
2022-09-08 16:42:37 +0800

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
 
 const kBottomContainerHeight = 80.0;
-const kActiveCardColour = Color(0xFF1D1E33);
-const kInactiveCardColour = Color(0xFF111328);
-const kBottomContainerColour = Color(0xFFEB1555);
+const kActiveCardColour = Color(0xFF0A0E21);
+const kInactiveCardColour = Color(0xFF1D1E33);
+const kBottomContainerColour = Color(0xFFFBC02D);
+
+const kHeadTextStyle = TextStyle(
+  color: Color(0xFFFBC02D),
+);
 
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
-  color: Color(0xFF8D8E98),
+  color: Color(0xFFFBC02D),
 );
 
 const kNumberTextStyle = TextStyle(
   fontSize: 50.0,
   fontWeight: FontWeight.w900,
+  color: Color(0xFFFBC02D),
 );
 
 const kLargeButtonTextStyle = TextStyle(
   fontSize: 25.0,
   fontWeight: FontWeight.bold,
+  color: Colors.black,
 );
 
 const kTitleTextStyle = TextStyle(
   fontSize: 50.0,
   fontWeight: FontWeight.bold,
+  color: Color(0xFFFBC02D),
 );
 
 const kResultTextStyle = TextStyle(
@@ -34,8 +41,10 @@ const kResultTextStyle = TextStyle(
 const kBMITextStyle = TextStyle(
   fontSize: 100.0,
   fontWeight: FontWeight.bold,
+  color: Color(0xFFFBC02D),
 );
 
 const kBodyTextStyle = TextStyle(
   fontSize: 22.0,
+  color: Color(0xFFFBC02D),
 );
